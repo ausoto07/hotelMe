e86078c3bcfeb447297387e37ea406a63294c942
Alex <ausoto07@gmail.com>
2017-12-30 12:37:14 -0500

    added hirb gem for rails console, and created seed file

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,13 @@
 #app/models/hotel.rb
 class Hotel < ApplicationRecord
-
-
+  # require name, location
+validates :name, presence: true
+  # def self.welcome_message
+  # "Welcome to our Hotel!"
+  # end
+  #
+  # def room_service
+  #   "This is room service, how may I help you?"
+  # end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hotel.create(name:"exlora Chile", location:"Patagonia chile")
+
+puts " explora chile created"
+
+peninsula = Hotel.new
+peninsula.name = "the peninsula hongkong"
+peninsula.location = "hong kong"
+peninsula.save
+
+puts "hotels puts in database"
+
+
+Hotel.create(name: "Four Seasons Whistler", location: "Whistler, Canada")
+Hotel.create(name: "Motel 8", location: "Waukesha, WI")
+
+Hotel.create(name: "B Ocean", location: "Lake Buena Vista, FL")
+puts "Hotel: B Ocean created"
+
+meridien = Hotel.new
+meridien.name = "Le Meridien"
+
+Hotel.create(name: "Ritz Carlton", location: "Grove, Miami")
+
+Hotel.create(name:"Setai Miami", location:"Miami, Florida")
